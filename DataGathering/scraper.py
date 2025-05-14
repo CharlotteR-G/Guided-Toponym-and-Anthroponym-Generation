ae4686d98252dbf8345fb80419ed5fd39e5196fd
CharlotteR-G <107860202+CharlotteR-G@users.noreply.github.com>
2025-05-14 18:22:03 +0100

    Scraper improved to get names by gender and culture
    Scraper will search by through male and female names of cultures in list and add them to the csv file. Limited culture list for now

--- DataGathering/scraper.py
+++ DataGathering/scraper.py
@@ -1,31 +1,34 @@
 import requests
 import csv
 
-# GENDERS = ["female given name", "male given name"]
-
 GENDERS = {
-    "female given name": "Q11879590",
-    "male given name": "Q12308941"
+    "Female": "Q11879590",
+    "Male": "Q12308941"
 }
 
-# cultureQualifiers = {
-#     "english": "Q1860"
-# }
+CULTURES = {
+    "English": "Q1860",
+    "German": "Q1860",
+    "Spanish": "Q1321",
+    "French": "Q150"
+}
 
-# URL = f"https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=SELECT%20DISTINCT%20%3FitemLabel%0AWHERE%20%7B%0A%20%20%20%20%3Fitem%20wdt%3AP31%20wd%3AQ11879590%3B%20%23%20Any%20instance%20of%20{GENDER}%0A%20%20%20%20%20%20%20%20%20%20wdt%3AP282%20wd%3AQ8229%3B%20%23%20latin%20writing%20script%0A%20%20%20%20%20%20%20%20%20%20wdt%3AP407%20wd%3AQ1860%3B%20%23%20With%20the%20property%20of%20english%20origin%0A%20%20%20%20%20%20%20%20%20%20wikibase%3Asitelinks%20%3Fsitelinks.%0A%0A%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cmul%2Cen%22%20%7D%0A%7D%0AORDER%20BY%20DESC%28%3Fsitelinks%29"
-
-with open('names.csv', 'w', newline='') as csvfile:
-    fieldnames = ['given name', 'gender']
+with open("names.csv", "w", newline="") as csvfile:
+    fieldnames = ["given name", "gender", "culture"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
     writer.writeheader()
 
-    for key, value in GENDERS.items():
-        response = requests.get(f"https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=SELECT%20DISTINCT%20%3FitemLabel%0AWHERE%20%7B%0A%20%20%20%20%3Fitem%20wdt%3AP31%20wd%3A{value}%3B%20%23%20Any%20instance%20of%20{key}%0A%20%20%20%20%20%20%20%20%20%20wdt%3AP282%20wd%3AQ8229%3B%20%23%20latin%20writing%20script%0A%20%20%20%20%20%20%20%20%20%20wdt%3AP407%20wd%3AQ1860%3B%20%23%20With%20the%20property%20of%20english%20origin%0A%20%20%20%20%20%20%20%20%20%20wikibase%3Asitelinks%20%3Fsitelinks.%0A%0A%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cmul%2Cen%22%20%7D%0A%7D%0AORDER%20BY%20DESC%28%3Fsitelinks%29")
-        if response.status_code == 200:
-            response = response.json() # data returned is a list of ‘repository’ entities
-            length = len(response["results"]["bindings"])
-            for i in range(length):
-                writer.writerow({"given name": response["results"]["bindings"][i]["itemLabel"]["value"],
-                                "gender": key})
+    # Loop through cultures and genders
+    for cultureKey, cultureValue in CULTURES.items():
+        for genderKey, genderValue in GENDERS.items():
+            response = requests.get(f"https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=SELECT%20DISTINCT%20%3FitemLabel%0AWHERE%20%7B%0A%20%20%20%20%3Fitem%20wdt%3AP31%20wd%3A{genderValue}%3B%20%23%20Any%20instance%20of%20{genderKey}%0A%20%20%20%20%20%20%20%20%20%20wdt%3AP282%20wd%3AQ8229%3B%20%23%20latin%20writing%20script%0A%20%20%20%20%20%20%20%20%20%20wdt%3AP407%20wd%3A{cultureValue}%3B%20%23%20With%20the%20property%20of%20english%20origin%0A%20%20%20%20%20%20%20%20%20%20wikibase%3Asitelinks%20%3Fsitelinks.%0A%0A%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cmul%2Cen%22%20%7D%0A%7D%0AORDER%20BY%20DESC%28%3Fsitelinks%29")
+            
+            if response.status_code == 200:
+                response = response.json() # data returned is a list of ‘repository’ entities
+                length = len(response["results"]["bindings"])
+
+                for i in range(length):
+                    writer.writerow({"given name": response["results"]["bindings"][i]["itemLabel"]["value"],
+                                    "gender": genderKey,
+                                    "culture": cultureKey})
 
