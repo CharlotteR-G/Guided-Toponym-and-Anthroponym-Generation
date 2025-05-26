15c704bfbb0452c93284716c5d79f44f65d545d6
CharlotteR-G <107860202+CharlotteR-G@users.noreply.github.com>
2025-05-27 00:32:00 +0100

    Inefficient bulk data scraper
    Had to change bulk scraper as the data wasn't being added to the file in a usable format. Current code is inefficient, slow and uncompressed but the data in the file is usable. going to improve

--- DataGathering/BulkScraping/bulkScraper.py
+++ DataGathering/BulkScraping/bulkScraper.py
@@ -3,6 +3,8 @@ import ijson
 import json
 from decimal import Decimal
 
+BATCHSIZE = 1000
+
 # Deal with the Decimals in wikiData
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -10,13 +12,9 @@ class DecimalEncoder(json.JSONEncoder):
             return float(obj)
         # Let the base class default method raise the TypeError
         return super().default(obj)
-
-
-# Extracting every human and all relevant information to make
-# Scrppaing faster when finding more specific information
-# like names
+    
+# Extract all humans from WikiData bulk source
 def extract_humans(bulkWikidataPath, outputPath="humans.json"):
-    humans = []
     count = 0
     firstItem = True
 
@@ -25,7 +23,6 @@ def extract_humans(bulkWikidataPath, outputPath="humans.json"):
 
         with bz2.open(bulkWikidataPath, "rt") as data:
             for item in ijson.items(data, "item"):
-
                 # If type is property, move on
                 if item["type"] == "property":
                     continue
@@ -37,57 +34,115 @@ def extract_humans(bulkWikidataPath, outputPath="humans.json"):
                 # get id and check item is human
                 itemInstances = item.get("claims", {}).get("P31", [])
                 for instance in itemInstances:
-                    # go through all claims and find if nay match human
+                    # go through all claims and find if any match human
                     if instance.get("mainsnak", {}).get("datavalue", {}).get("value", {}).get("id") == "Q5":
+                        # if human
                         if not firstItem:
                             outfile.write(",")
-                        
-                        # Write human to file
                         json.dump(item, outfile, cls=DecimalEncoder, indent=2)
+
                         firstItem = False
+
                         count += 1
                         break
 
-                # conditional for testing
-                if count == 10:
+                if count % BATCHSIZE == 0:
+                    print(count)
+
+                if count == 5000:
                     break
-            
-            outfile.write("]")
 
-            # instances = claims.get("P31", [])
-            # for instance in instances:
-            #     id = instance.get("mainsnak", {}).get("datavalue", {}).get("value", {}).get("id")
-            #     # isHuman = True if id == "Q5" else False # Q5 == human
-
-            #     if id == "Q5": # if human
-            #         # append relevant data to
-
-            #         # Convert sex qualifier to human readable
-            #         # print(item.get("labels", {}).get("en", {}).get("value"))
-
-            #         # Ensure person has a sex listed
-            #         if not item.get("claims", {}).get("P21", []):
-            #             break
-
-            #         sexID = item.get("claims", {}).get("P21", [])[0].get("mainsnak", {}).get("datavalue", {}).get("value", {}). get("id")
-            #         sex = "male" if sexID == "Q6581097" else "female"
-
-            #         humanData = {
-            #             "id": item.get("id"),
-            #             "name": item.get("labels", {}).get("en", {}).get("value"),
-            #             "sex": sex
-            #         }
-
-            #         humans.append(humanData)
-            #         count += 1
-            #         break
-            # if count % 10000 == 0:
-            #     print(count)
-
-    # with open(outputPath, "w") as outfile:
-    #     json.dump(humans, outfile, indent=2)
-    #     print(count)
-
-            
+        outfile.write("]")
 
 extract_humans("latest-all.json.bz2")
+
+with open("humans.json", "r") as data:
+    for item in ijson.items(data, "item"):
+        print(item.get("labels", {}).get("en", {}).get("value"))
+
+
+
+
+
+# Extracting every human and all relevant information to make
+# Scrppaing faster when finding more specific information
+# like names
+# def extract_humans(bulkWikidataPath, outputPath="humans.json"):
+#     humanBatch = []
+#     count = 0
+#     firstItem = True
+
+#     with open(outputPath, "w") as outfile:
+
+#         with bz2.open(bulkWikidataPath, "rt") as data:
+#             for item in ijson.items(data, "item"):
+
+#                 # If type is property, move on
+#                 if item["type"] == "property":
+#                     continue
+
+#                 # make sure item has instance of property
+#                 if not item.get("claims", {}).get("P31", []):
+#                     continue
+
+#                 # get id and check item is human
+#                 itemInstances = item.get("claims", {}).get("P31", [])
+#                 for instance in itemInstances:
+#                     # go through all claims and find if any match human
+#                     if instance.get("mainsnak", {}).get("datavalue", {}).get("value", {}).get("id") == "Q5":
+#                         # if human
+#                         humanBatch.append(item)
+#                         count += 1
+#                         break
+                
+#                 # Every 1000 humans
+#                 if len(humanBatch) == BATCHSIZE:
+#                     if not firstItem:
+#                         outfile.write(",")
+#                     json.dump(humanBatch, outfile, cls=DecimalEncoder, indent=2)
+#                     firstItem = False
+#                     print(count)
+#                     humanBatch.clear()
+#                     if count >= 50:
+#                         break
+
+#             # handle final batch
+#             # if humanBatch:
+#             #     if not firstItem:
+#             #         outfile.write(",")
+#             #     json.dump(humanBatch, outfile, cls=DecimalEncoder, indent=2)
+
+#             # instances = claims.get("P31", [])
+#             # for instance in instances:
+#             #     id = instance.get("mainsnak", {}).get("datavalue", {}).get("value", {}).get("id")
+#             #     # isHuman = True if id == "Q5" else False # Q5 == human
+
+#             #     if id == "Q5": # if human
+#             #         # append relevant data to
+#             #         # Convert sex qualifier to human readable
+#             #         # print(item.get("labels", {}).get("en", {}).get("value"))
+
+#             #         # Ensure person has a sex listed
+#             #         if not item.get("claims", {}).get("P21", []):
+#             #             break
+
+#             #         sexID = item.get("claims", {}).get("P21", [])[0].get("mainsnak", {}).get("datavalue", {}).get("value", {}). get("id")
+#             #         sex = "male" if sexID == "Q6581097" else "female"
+
+#             #         humanData = {
+#             #             "id": item.get("id"),
+#             #             "name": item.get("labels", {}).get("en", {}).get("value"),
+#             #             "sex": sex
+#             #         }
+
+#             #         humans.append(humanData)
+#             #         count += 1
+#             #         break
+#             # if count % 10000 == 0:
+#             #     print(count)
+
+#     # with open(outputPath, "w") as outfile:
+#     #     json.dump(humans, outfile, indent=2)
+#     #     print(count)
+
+        
